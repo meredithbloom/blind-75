b0db19d9b7727b3204ea15908d1c7d92de790d41
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-07-17 15:56:15 -0400

    added second array blind 75 problem. unsolved

---
+++ arrays/best-time-to-buy-sell-stock.py
@@ -0,0 +1,19 @@
+# BEST TIME TO BUY AND SELL STOCK
+
+# Given an array prices where prices[i] is the price of a given stock on the ith day
+
+# You want to maximize profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock
+
+# Return the maximum profit you can achieve from this transation. If you cannot achieve any profit, return 0.
+
+# Constraints
+# 1 <= prices.length <= 10^5
+# 0 <= prices[i] <= 10^4
+
+
+# Examples
+
+# Input
+prices1 = [7,1,5,3,6,4]
+
+prices2 = [7,6,4,3,1]
