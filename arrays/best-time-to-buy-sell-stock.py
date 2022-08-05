f0beb6482be5bed8b5ee0ce6238c79d73eeab585
Meredith Bloom <31168207+meredithbloom@users.noreply.github.com>
2022-08-05 03:41:06 +0000

    partial solution to best time to buy and sell stock

--- arrays/best-time-to-buy-sell-stock.py
+++ arrays/best-time-to-buy-sell-stock.py
@@ -10,6 +10,8 @@
 # 1 <= prices.length <= 10^5
 # 0 <= prices[i] <= 10^4
 
+# Array, Dynamic Programming
+
 
 # Examples
 
@@ -17,8 +19,51 @@
 prices1 = [7,1,5,3,6,4]
 
 prices2 = [7,6,4,3,1]
+prices3 = [1,4,2]
+prices4 = [3,2,6,5,0,3]
 
+# brute force would be to compare every item with every item after it in the array - O(n^2)
+# should be able to get it down to O(n) by progressing through the list from both directions until they meet in the middle
+
+def difference(sell, buy):
+	if (sell-buy) < 0:
+		return 0
+	else:
+		return (sell - buy)
 
 
 def maxProfit(prices):
-    pass
+	print(prices)
+	# starting from each end and moving towards center
+	buy = 0
+	sell = len(prices)-1
+	#max_profit = 0
+	low = prices[buy]
+	low_index = 0
+	high = prices[len(prices)-1]
+	high_index = len(prices)-1
+	print(f'starting low is {low} and starting high is {high}')
+	# before buy and sell indices meet in the middle
+	while low_index < high_index:
+		print(f'current high index is {high_index} and current low index is {low_index}')
+		if prices[buy] < low and low_index < high_index:
+			print(f'current low index is {low_index}')
+			low = prices[buy]
+			low_index = buy
+			print(f'new lowest price is {low} at index {low_index}')
+		if prices[sell] > high and high_index > low_index:
+			high = prices[sell]
+			high_index = sell
+			print(f'new highest sell is {high} at index {high_index}')	
+		buy += 1
+		sell -= 1
+	# print(f'current high index is {high_index} and current low index is {low_index}')
+	print(difference(high, low))
+	
+	
+
+
+#maxProfit(prices1)
+#maxProfit(prices2)
+#maxProfit(prices3)
+maxProfit(prices4)
